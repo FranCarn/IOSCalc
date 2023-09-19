05221702d28454c379e79cf8c3ca9d40b0877fac
@FranCarn <carnevalefranco99@gmail.com>
2023-09-19 15:36:29 -0300

    all buttons logic ready

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,14 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import {Text, View} from 'react-native';
 import {styles} from '../theme/appTheme';
 import {ButtonCalc} from '../components/ButtonCalc';
+
+enum Operators {
+  addition,
+  substract,
+  multiply,
+  split,
+}
 
 export const CalculatorScreen = () => {
   const [number, setNumber] = useState<string>('0');
 
   const [prevNumber, setPrevNumber] = useState<string>('0');
 
-  const deleteAllEntries = () => setNumber('0');
+  const lastOperator = useRef<Operators>();
+
+  const deleteAllEntries = () => {
+    setNumber('0');
+    setPrevNumber('0');
+  };
 
   const makeNumber = (numberText: string) => {
     if (number.includes('.') && numberText === '.') return;
@@ -27,6 +39,16 @@ export const CalculatorScreen = () => {
     } else {
       setNumber(number + numberText);
     }
+  };
+
+  const changeNumberForLastNumber = () => {
+    if (number.endsWith('.')) {
+      setPrevNumber(number.slice(0, -1));
+    } else {
+      setPrevNumber(number);
+    }
+
+    setNumber('0');
   };
 
   const deleteLastEntry = () => {
@@ -51,9 +73,29 @@ export const CalculatorScreen = () => {
     }
   };
 
+  const splitButton = () => {
+    changeNumberForLastNumber();
+    lastOperator.current = Operators.split;
+  };
+  const multiplyButton = () => {
+    changeNumberForLastNumber();
+    lastOperator.current = Operators.multiply;
+  };
+  const substractButton = () => {
+    changeNumberForLastNumber();
+    lastOperator.current = Operators.substract;
+  };
+  const additionButton = () => {
+    changeNumberForLastNumber();
+    lastOperator.current = Operators.addition;
+  };
+
   return (
     <View style={styles.calcContainer}>
-      <Text style={styles.valueSmall}>{prevNumber}</Text>
+      {prevNumber !== '0' && (
+        <Text style={styles.valueSmall}>{prevNumber}</Text>
+      )}
+
       <Text style={styles.totalValue} numberOfLines={1} adjustsFontSizeToFit>
         {number}
       </Text>
@@ -61,25 +103,25 @@ export const CalculatorScreen = () => {
         <ButtonCalc text="C" color="#9b9b9b" action={deleteAllEntries} />
         <ButtonCalc text="+/-" color="#9b9b9b" action={positiveNegative} />
         <ButtonCalc text="DEL" color="#9b9b9b" action={deleteLastEntry} />
-        <ButtonCalc text="/" color="#FF9427" action={deleteAllEntries} />
+        <ButtonCalc text="/" color="#FF9427" action={splitButton} />
       </View>
       <View style={styles.row}>
         <ButtonCalc text="7" action={makeNumber} />
         <ButtonCalc text="8" action={makeNumber} />
         <ButtonCalc text="9" action={makeNumber} />
-        <ButtonCalc text="X" color="#FF9427" action={deleteAllEntries} />
+        <ButtonCalc text="X" color="#FF9427" action={multiplyButton} />
       </View>
       <View style={styles.row}>
         <ButtonCalc text="4" action={makeNumber} />
         <ButtonCalc text="5" action={makeNumber} />
         <ButtonCalc text="6" action={makeNumber} />
-        <ButtonCalc text="-" color="#FF9427" action={deleteAllEntries} />
+        <ButtonCalc text="-" color="#FF9427" action={substractButton} />
       </View>
       <View style={styles.row}>
         <ButtonCalc text="1" action={makeNumber} />
         <ButtonCalc text="2" action={makeNumber} />
         <ButtonCalc text="3" action={makeNumber} />
-        <ButtonCalc text="+" color="#FF9427" action={deleteAllEntries} />
+        <ButtonCalc text="+" color="#FF9427" action={additionButton} />
       </View>
       <View style={styles.row}>
         <ButtonCalc text="0" stretch action={makeNumber} />
